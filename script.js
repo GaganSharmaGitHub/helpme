2cd87cbec9d59f25bc69ed1e4499008871e79a1e
Gagan Sharma <43646043+codeongags@users.noreply.github.com>
2019-10-13 13:34:44 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+//scroll
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 500) {
+
+        document.getElementById("stickytop").style.display = "block";
+
+    } else {
+        document.getElementById("stickytop").style.display = "none";
+        document.getElementById("sidebar").style.width = "0px";
+    }
+}
+//menu icon
+function menuicon(x) {
+    x.classList.toggle("change");
+    if (document.getElementById("sidebar").style.width === "300px") {
+        document.getElementById("sidebar").style.width = "0px"
+    } else { document.getElementById("sidebar").style.width = "300px" }
+}
