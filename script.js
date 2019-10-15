07d4f23fb20d1ee229238764d9b788691e6b6cd8
Gagan Sharma <43646043+codeongags@users.noreply.github.com>
2019-10-16 02:40:02 +0530

    edit header

--- script.js
+++ script.js
@@ -2,21 +2,37 @@
 window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 500) {
+    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 150) {
 
         document.getElementById("stickytop").style.display = "block";
 
     } else {
         document.getElementById("stickytop").style.display = "none";
-        document.getElementById("sidebar").style.width = "0px";
+        document.getElementById("sidebar").style.display = "none";
+    }
+}
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
     }
 }
 //menu icon
 function menuicon(x) {
     x.classList.toggle("change");
-    if (document.getElementById("sidebar").style.width === "300px") {
-        document.getElementById("sidebar").style.width = "0px"
-    } else { document.getElementById("sidebar").style.width = "300px" }
+    if (document.getElementById("sidebar").style.display === "block") {
+
+        document.getElementById("sidebar").classList.add("slideOutLeft");
+        document.getElementById("sidebar").style.display = "inline-block";
+
+    } else {
+        document.getElementById("sidebar").style.display = "block"
+
+        document.getElementById("sidebar").classList.remove("slideOutLeft");
+    }
 }
 
 function expand(x) {
@@ -24,12 +40,18 @@ function expand(x) {
 
     document.getElementById(taskslist[x]).style.display = "block";
 
-    document.getElementById(taskslist[x]).classList.remove("slideOutDown");
+    document.getElementById(taskslist[x]).classList.remove("slideOutLeft");
 }
 
-function close(x) {
+function closed(x) {
     var taskslist = ["task1", "task2", "task3"];
 
-    document.getElementById(taskslist[x]).style.display = "none";
-    document.getElementById(taskslist[x]).classList.add("slideOutDown");
+
+    document.getElementById(taskslist[x]).classList.add("slideOutLeft");
 }
+
+
+
+//document.querySelector(".close").addEventListener("click", function() {
+//  $(".task").fadeOut(2000)
+//});
