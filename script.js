372d8f5dd96affef78712db0003f2080443cf29b
Gagan Sharma <43646043+codeongags@users.noreply.github.com>
2019-12-28 17:22:04 +0530

    post task form working

--- script.js
+++ script.js
@@ -40,11 +40,23 @@ function expand(x) {
 
     document.getElementById("taskPopUp").style.display = "block";
     document.getElementById("taskPopUp").classList.remove("slideOutLeft");
+    console.log(x);
 }
 
-function closed(x) {
+function formPopUp() {
+
+    document.getElementById("formPopUp").style.display = "block";
+    document.getElementById("formPopUp").classList.remove("slideOutLeft");
+}
+
+function closed() {
 
     document.getElementById("taskPopUp").classList.add("slideOutLeft");
+}
+
+function closeForm() {
+
+    document.getElementById("formPopUp").classList.add("slideOutLeft");
 }
 
 function taskData(title, company, skills, date, reward) {
@@ -55,19 +67,54 @@ function taskData(title, company, skills, date, reward) {
     this.reward = reward || 0;
     this.date = date;
 }
-var taskData1 = new taskData("Graphic Design", "Iris Media", "Graphics", "March", 100);
-var taskData2 = new taskData("Testing", "Titus Tech", "JavaScript", "April", 200);
-var taskData3 = new taskData("C# coding", "TigerOne", "C#", "January", 200);
-var taskData4 = new taskData("python debugging", "Titus Tech", "python", "February", 250);
+
+var data = [];
+data[0] = new taskData("Graphic Design", "Iris Media", "Graphics", "March", 100);
+data[1] = new taskData("Testing", "Titus Tech", "JavaScript", "April", 200);
+data[2] = new taskData("C# coding", "TigerOne", "C#", "January", 200);
+data[3] = new taskData("python debugging", "Titus Tech", "python", "February", 250);
 
 function writeData(x) {
-    var tasksDataList = [taskData1, taskData2, taskData3, taskData4];
-    document.getElementById("taskDataTitle").innerHTML = tasksDataList[x].title;
-    document.getElementById("taskDataCompany").innerHTML = tasksDataList[x].company;
-    document.getElementById("taskDataSkills").innerHTML = tasksDataList[x].skills;
-    document.getElementById("taskDataDate").innerHTML = tasksDataList[x].date;
-    document.getElementById("taskDataReward").innerHTML = tasksDataList[x].reward;
+    document.getElementById("taskDataTitle").innerHTML = data[x].title;
+    document.getElementById("taskDataCompany").innerHTML = data[x].company;
+    document.getElementById("taskDataSkills").innerHTML = data[x].skills;
+    document.getElementById("taskDataDate").innerHTML = data[x].date;
+    document.getElementById("taskDataReward").innerHTML = data[x].reward;
 
     document.getElementById("companyImage").src = "res/" +
-        tasksDataList[x].company + ".png";
+        data[x].company + ".png";
+}
+
+function submitt() {
+    var titleForm = document.getElementById("titleForm").value;
+    var companyForm = document.getElementById("companyForm").value;
+    var skillsForm = document.getElementById("skillsForm").value;
+    var dateForm = document.getElementById("dateForm").value;
+    var rewardsForm = document.getElementById("rewardsForm").value;
+    var detailsForm = document.getElementById("detailsForm").value;
+    var num = data.push(new taskData(titleForm, companyForm, skillsForm, dateForm, rewardsForm));
+    for (var i in data) {
+        console.log(data[i].title + i);
+    }
+    post(num - 1);
+    closeForm();
+}
+
+function post(num) {
+    const parent = document.getElementById("postParent");
+    let newPost = document.createElement('div');
+    newPost.classList.add("card", "col-sm-12", "col-md-3");
+    newPost.innerHTML = '<img class="card-img-top" src="res/' +
+        data[num].company + '.png" alt="Card image" style="width:100%">' + '<div class="card-body">' +
+        '<h4 class="card-title">' + data[num].title + '</h4><p class="card-text">' + data[num].company + '</p>' +
+        '<a style="text-decoration: none;" class="btn btn-primary">' +
+        '<ion-icon name="log-in"></ion-icon>Apply</a>' +
+        '<a class="btn btn-primary">' +
+        '<ion-icon name="share"></ion-icon>Share</a>' +
+        '<a onclick="expand(' +
+        num +
+        ')" class="btn btn-primary">' +
+        '<ion-icon class="fa fa-info-circle"></ion-icon>Details</a>' +
+        '</div>'
+    parent.appendChild(newPost);
 }
